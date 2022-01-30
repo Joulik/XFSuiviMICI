2faf1214fa6798faeb4ddd587307396ac2867822
Joulik <leroy.prof.spc@gmail.com>
2022-01-30 21:01:18 +0100

    Added Model for Daily and installed sqlite-net-pcl

---
+++ XFSuiviMICI/XFSuiviMICI/Models/DailyReport.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFSuiviMICI.Models
+{
+    public class DailyReport
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int BowelMouvement { get; set; }
+        public DateTime Date { get; set; }
+        public int Diastolic { get; set; }
+        public bool FeelingPain { get; set; }
+        public bool HavingDiarrhea { get; set; }
+        public int HeartRate { get; set; }
+        public int Systolic { get; set; }
+        public int Tiredness { get; set; }
+    }
+}
