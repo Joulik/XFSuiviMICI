6b2ebf5cce4664ca6d3877d382c0c9701afce4e1
Joulik <leroy.prof.spc@gmail.com>
2022-02-22 15:13:28 +0100

    Implemented NewLabWork MVVM

---
+++ XFSuiviMICI/XFSuiviMICI/ViewModels/NewLabWorkVM.cs
@@ -0,0 +1,50 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using XFSuiviMICI.Models;
+
+namespace XFSuiviMICI.ViewModels
+{
+    public class NewLabWorkVM
+    {
+        public Command SaveCommand { get; set; }
+
+        public DateTime DateLabWork { get; set; }
+        public string TestName { get; set; }
+        public string TestValue { get; set; }
+        public string TestUnit { get; set; }
+
+        public NewLabWorkVM()
+        {
+            DateLabWork = DateTime.Now;
+            SaveCommand = new Command(Save);
+        }
+
+        private void Save()
+        {
+            LabWork labWork = new LabWork
+            {
+                DateLabWork = DateLabWork,
+                TestName = TestName,
+                TestValue = float.Parse(TestValue, CultureInfo.InvariantCulture.NumberFormat),
+                TestUnit = TestUnit,
+            };
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+
+            conn.CreateTable<LabWork>();
+            int rows = conn.Insert(labWork);
+            conn.Close();
+
+            if (rows > 0)
+                App.Current.MainPage.DisplayAlert("", "Sauvegarde effectuée", "OK");
+            else
+                App.Current.MainPage.DisplayAlert("Problème", "Sauvegarde non enregistré", "OK");
+
+            App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
---
+++ XFSuiviMICI/XFSuiviMICI/Views/NewLabWorkPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFSuiviMICI.Models;
+
+namespace XFSuiviMICI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewLabWorkPage : ContentPage
+    {
+        public NewLabWorkPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
