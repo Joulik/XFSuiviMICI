72db8e8903f5288c7b89f7ee9b66293e59818e3d
Joulik <leroy.prof.spc@gmail.com>
2022-03-03 16:41:19 +0100

    Implemented Converters in TreatmentPage

---
+++ XFSuiviMICI/XFSuiviMICI/Helpers/TreatmentBoolToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XFSuiviMICI.Helpers
+{
+    public class TreatmentBoolToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string statusText;
+
+            if (Equals(value,true))
+                statusText = "suspendu ou arrêté";
+            else
+                statusText = "en cours depuis le";
+                
+            return statusText;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
